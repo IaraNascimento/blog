cf03a2bf83078e0e007f685f82073f50a4d24cd1
Iara <iara.sztybe@gmail.com>
2021-05-09 22:50:12 -0300

    melhorando página de about

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -8,7 +8,37 @@
                 <blockquote class="about-quote">Queria mesmo era ser mestre pokémon, mas mal consegui treinar os pombos do parque.</blockquote>
             </aside>
             <div class="about-content">
-                <p>Líder e Desenvolvedora especializada em front-end com 6 anos de experiência. Entusiasta de metodologias ágeis, automação, testes e códigos elegantes. Meu ambiente de trabalho ideal é transparente, descentralizado, descontraído, colaborativo, flexível e receptivo à inovação.</p>
+                <h2><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'laptop' }" /> Profissional</h2>
+                <p>Líder e Desenvolvedora especializada em front-end com 6 anos de experiência. Entusiasta de metodologias ágeis, automação, testes e códigos elegantes.</p>
+                <p>Possuo inteligência emocional, empatia, ética, postura de liderança, e flexibilidade. No que se diz respeito a habilidades técnicas, possuo expertise com:</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Angular JS e Angular 2+</li>
+                    <li>React</li>
+                    <li>Vue</li>
+                    <li>Node</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>SASS</li>
+                    <li>LESS</li>
+                    <li>Bootstrap</li>
+                    <li>Materialize</li>
+                    <li>GoogleCharts</li>
+                    <li>FontAwesome</li>
+                    <li>REST</li>
+                    <li>MongoDB</li>
+                    <li>Git</li>
+                    <li>Programação Funcional</li>
+                    <li>Programação Reativa</li>
+                    <li>Metodologias Ágeis</li>
+                </ul>
+                <p>Meu ambiente de trabalho ideal é transparente, descentralizado, descontraído, colaborativo, flexível e receptivo à inovação.</p>
+                <h2><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'user-graduate' }" /> Formação</h2>
+                <p>Iniciei duas faculdades mas tranquiei ambas. A primeira foi Ciências Atmosféricas na USP e a segunda foi Análise e Desenvolvimento de Sistemas na FATEC.</p>
+                <p>Pretendo fazer uma EAD de Tecnólogo em Análise e Desenvolvimento de Sistemas na Impacta.</p>
+                <h2><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'coffee' }" /> Pessoal</h2>
+                <p>Toda vida em São Paulo. Dois irmãos por parte de mãe e dois irmãos por parte de pai. Medicada e com terapia em dia.</p>
                 <p>Fã de GOT, Scrubs, Drag Race e Friends. Também gosto de livros de ficção e de animes. Assisto as mesmas coisas inúmeras vezes! Gosto de tocar violão e de jogar TCG de Pokémon e Magic. Meu rolê preferido é sair para comer e conversar com amigos.</p>
             </div>
         </div>
@@ -16,6 +46,11 @@
 </template>
 
 <script>
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserGraduate, faLaptop, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserGraduate, faLaptop, faCoffee);
 
 export default {
 
@@ -76,12 +111,25 @@ $aside-width: 280px;
     }
     &-content {
         margin-bottom: 32px;
+        h2 {
+            font-size: 22px;
+            margin-bottom: 16px;
+        }
         p {
+            font-size: 16px;
             line-height: 20px;
-            margin-bottom: 8px;
+            margin-bottom: 16px;
             text-align: justify;
             &:first-letter {
                 margin-left: 36px;
+            }
+        }
+        ul {
+            display: flex;
+            flex-wrap: wrap;
+            margin-bottom: 16px;
+            li {
+                margin: 0 16px 8px 0;
             }
         }
     }
